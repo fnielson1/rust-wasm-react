460f6980c0be9c1e369ef22c6cf51169a16ed4f6
fnielson1 <f.nielson@gmail.com>
2025-11-09 13:28:31 -0700

    feat(setup): removed unnecessary server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,8 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8080;
 
-const webDir = path.join(__dirname, '../../web');
-
-app.use(express.static(webDir));
 app.use((express.json()));
 
 app.get('/alive', (req, res) => {
